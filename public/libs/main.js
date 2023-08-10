f76240064af2711ec1f9918aa2e8c93665c9b735
amin <amincxo@gmail.com>
2023-08-11 00:16:42 +0330

    fix problme 3d

---
+++ public/libs/main.js
@@ -0,0 +1,35 @@
+import {loadGLTF, loadVideo} from "../../libs/loader.js";
+// import {mockWithVideo} from '../../libs/camera-mock';
+import {createChromaMaterial} from './chroma-video.js';
+
+const THREE = window.MINDAR.IMAGE.THREE;
+
+document.addEventListener('DOMContentLoaded', () => {
+  const start = async() => {
+    // mockWithVideo('../../assets/mock-videos/course-banner1.mp4');
+    
+    const mindarThree = new window.MINDAR.IMAGE.MindARThree({
+      container: document.body,
+      imageTargetSrc: '../targets.mind',
+      maxTrack:1,
+    });
+    const {renderer, scene, camera} = mindarThree;
+
+    const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
+    scene.add(light);
+
+    const raccoon = await loadGLTF('../3dmodel.gltf');
+    raccoon.scene.scale.set(0.1, 0.1, 0.1);
+    raccoon.scene.position.set(0, -0.4, 0);
+
+    const anchor = mindarThree.addAnchor(0);
+    anchor.group.add(raccoon.scene);
+
+
+    await mindarThree.start();
+    renderer.setAnimationLoop(() => {
+      renderer.render(scene, camera);
+    });
+  }
+  start();
+});
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const fs = require('fs');
+const https = require('https');
+const cheerio = require('cheerio');
+
+const app = express();
+
+
+const privateKey = fs.readFileSync('cetificates/key.pem', 'utf8');
+const certificate = fs.readFileSync('cetificates/cert.pem', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+
+
+// تعریف فایل استاتیک
+app.use(express.static('public'));
+
+// روت کردن فایل
+app.get('/', (req, res) => {
+  fs.readFile('./public/index.html', (err, data) => {
+    if (err) throw err;
+    const $ = cheerio.load(data);
+    const pageTitle = $('title').text();
+    res.send(`Page Title: ${pageTitle}`);
+  });
+});
+
+const httpsServer = https.createServer(credentials, app);
+
+
+httpsServer.listen(3001, () => {
+  console.log('HTTPS Server running on port 3001');
+});
+
